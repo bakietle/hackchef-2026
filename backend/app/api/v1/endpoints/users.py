202f6060cf26488c02f6ff614682bee17edf8c69
Ba Kiet Le <253419590+bakietle@users.noreply.github.com>
2026-03-14 22:33:00 +0800

    backend everything updated

--- backend/app/api/v1/endpoints/users.py
+++ backend/app/api/v1/endpoints/users.py
@@ -1,15 +1,18 @@
 from uuid import uuid4
 
-from fastapi import APIRouter, status
-
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.orm import Session
+from app.db.session import get_db
 from app.schemas.user import UserCreate, UserResponse
+from app.models.users import User  
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-def create_user(user: UserCreate):
-    return {
-        "id": uuid4(),
-        "username": user.username,
-    }
+def create_user(user: UserCreate,db: Session = Depends(get_db)):
+    db_user = User(username=user.username)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user

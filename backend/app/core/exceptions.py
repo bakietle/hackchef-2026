a4ccb7474cd6c693d008e14309c989eb6fd0e853
Nhat Minh Do <140363490+zephris@users.noreply.github.com>
2026-03-14 20:02:05 +0800

    added global Modes and removed global cuisines, meal_types, modes, status_strings from constants.py. Added placeholder methods for UserAlreadyExistsError and InvalidCredentialsError in exceptions.py.

--- backend/app/core/exceptions.py
+++ backend/app/core/exceptions.py
@@ -11,3 +11,12 @@
 #Why useful?
 
 #Keeps service code cleaner later.
+
+class Exceptions:
+    def UserAlreadyExistsError(self, email: str):
+        return None
+    
+    def InvalidCredentialsError(self):
+        return None
+    
+    
